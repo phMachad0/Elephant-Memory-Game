973f55fefdcd9b6a48a0e1c8f12a9fb41a7b109a
brunoVA101 <brunovalcantara@hotmail.com>
2023-03-31 22:04:32 -0300

    ✅ Correção do random_generator

--- random_generator_5bits.vhd
+++ random_generator_5bits.vhd
@@ -1,15 +1,17 @@
 library ieee;
-use ieee.std_logic_1164;
+use ieee.std_logic_1164.all;
 
 
 entity random_generator_5bits is
-    clock: in std_logic;
-    enable: in std_logic;
-    dado: out std_logic_vector(4 downto 0);
+    port(
+        clock: in std_logic;
+        enable: in std_logic;
+        dado: out std_logic_vector(4 downto 0)
+    );
 end entity random_generator_5bits;
 
 architecture LFSR of random_generator_5bits is
-    signal dado28: std_logic_vector(27 downto 0) := "1010110101101011010110101101"
+    signal dado28: std_logic_vector(27 downto 0) := "1010110101101011010110101101";
     signal dado28prox: std_logic_vector(27 downto 0);
 
 begin
@@ -24,11 +26,11 @@ begin
             dado28prox(i+1) <= dado28(i);
         end generate;
 
-    dado28prox(3) <= dado28(27) xor dado(2);
+    dado28prox(3) <= dado28(27) xor dado28(2);
 
-    dado28pro(2) <= dado28(1);
-    dado28pro(1) <= dado28(0);
-    dado28pro(0) <= dado28(27);
+    dado28prox(2) <= dado28(1);
+    dado28prox(1) <= dado28(0);
+    dado28prox(0) <= dado28(27);
 
     dado <= dado28(4 downto 0);
 end architecture;
